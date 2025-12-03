7fa8420fd6c3f07c4c2bc2c8485fc36fe75f45f8
kamrulhasansamol <kamrulhasansamol.bd@gmail.com>
2025-12-03 20:36:41 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os 
+
+
+
+AES_KEY = b'Yg&tc%DEuh6%Zc^8'
+AES_IV = b'6oyZDr22E3ychjM%'
+
+PORT = 5010
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from flask_caching import Cache
+from app.utils.response import process_token
+from colorama import init
+import warnings
+from urllib3.exceptions import InsecureRequestWarning
+import time
+from config import PORT
+# Ignore SSL warnings
+warnings.filterwarnings("ignore", category=InsecureRequestWarning)
+from pprint import pprint
+# Initialize colorama
+init(autoreset=True)
+
+# Initialize Flask app
+app = Flask(__name__)
+
+cache = Cache(app, config={"CACHE_TYPE": "simple"})  # In-memory cache
+
+@app.route("/")
+def home():
+    return "JWT Token Generator API is running!"
+
+@app.route("/token", methods=["GET"])
+def get_responses():
+    uid = request.args.get("uid")
+    password = request.args.get("password")
+
+    if uid and password:
+        # Generate a unique cache key per request using a timestamp
+        cache_key = f"token_{uid}_{password}_{int(time.time())}"
+
+        response = process_token(uid, password)
+        status_code =response.get("status_code")
+        # pprint(response)
+        cache.set(
+            cache_key, response, timeout=25200
+        )  # Cache response with 7-hour expiry
+        return jsonify(response),status_code
+
+    # Bulk retrieval logic removed as per request
+    return jsonify({"message": "Bulk retrieval logic has been removed."})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
